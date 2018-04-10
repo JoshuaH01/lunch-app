d85403f12ccadf09c8cc1696235487ca3fabd549
Joshua Hill <joshua.hill@digital.hmrc.gov.uk>
2018-04-10 15:08:04 +0100

    Complete lesson 4

--- app/services/GreetingService.scala
+++ app/services/GreetingService.scala
@@ -3,14 +3,20 @@
 package services
 
 
-class GreetingService {
-    def greeting: String = ???
-
-}
+import com.google.inject.ImplementedBy
 
 class RealGreetingService extends GreetingService {
-    override def greeting: String = ???
+  def greeting: String = {
+    val now = Calendar.getInstance()
+    val currentHour = now.get(Calendar.HOUR_OF_DAY)
+    if (currentHour < 12)
+      "Good morning!"
+    else
+      "Good afternoon!"
+  }
 }
 
-
-
+@ImplementedBy(classOf[RealGreetingService])
+trait GreetingService {
+  def greeting: String
+}
